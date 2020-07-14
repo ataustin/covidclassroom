a503e8380088b82ef485f4b175256ea89ee201da
Adam Austin <ataustin@gmail.com>
2020-07-14 17:11:03 -0500

    squash bugs

--- server.R
+++ server.R
@@ -1,14 +1,24 @@
 server <- function(input, output) {
   output$classroom_map <-
-    plot_classroom(buffer_front = input$buffer_front,
-                   buffer_rear  = input$buffer_rear,
-                   buffer_left  = input$buffer_left,
-                   buffer_right = input$buffer_right,
-                   pupil_clearance = input$pupil_clearance,
-                   desk_width      = input$desk_width,
-                   desk_depth      = input$desk_depth,
-                   room_width      = input$room_width,
-                   room_depth      = input$room_depth,
-                   room_number     = input$room_number,
-                   min_desk_separation = input$min_desk_separation)
+  renderPlot(
+    {
+      pl <- plot_classroom(
+        buffer_front = input$buffer_front,
+        buffer_rear  = input$buffer_rear,
+        buffer_left  = input$buffer_left,
+        buffer_right = input$buffer_right,
+        pupil_clearance = input$pupil_clearance,
+        desk_width  = input$desk_width,
+        desk_depth  = input$desk_depth,
+        room_width  = input$room_width,
+        room_depth  = input$room_depth,
+        room_number = input$room_number,
+        min_desk_separation = input$min_desk_separation
+      )
+
+      pl
+    },
+    width = function() input$room_width * 15,
+    height = function() input$room_depth * 15
+  )
 }
